1c70fc221e5beff8db44951d45827068baba167c
terryblue99 <terryblue@comcast.net>
2017-05-03 15:30:36 -0500

    added require_relative for scraper

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative '../lib/coming_soon/version'
 require_relative '../lib/coming_soon/movie'
 require_relative '../lib/coming_soon/cli'
+require_relative '../lib/coming_soon/scraper'
