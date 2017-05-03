2340b05d50270d3c662cea91105afc8225b9d82a
terryblue99 <terryblue@comcast.net>
2017-05-03 18:51:30 -0500

    Refactored

--- lib/coming_soon/scraper.rb
+++ lib/coming_soon/scraper.rb
@@ -1,6 +1,6 @@
 class ComingSoon::Scraper
 
-	def self.scrape_movies
+	def scrape_movies
 
 		doc = Nokogiri::HTML(open("http://www.fandango.com/moviescomingsoon"))
 		# name: doc.css("li.visual-item a.visual-title").text.strip
@@ -17,38 +17,48 @@ class ComingSoon::Scraper
 			soon.start_date = movie.css("span").text
 			soon.url = movie.css("a").attribute("href").value
 
-			self.scrape_synopsis(soon)
+			scrape_details(soon)
 
 			count+=1
-			if count > 19 # Displays only 20 movies
+			if count > 19 # Scrapes only 20 movies
 				break
 			end
 		end
 		
 	end
 
-	def self.scrape_synopsis(soon)
-
+	def scrape_details(soon)
+		
 		redirect_failed = false
 
 		begin
-			@doc_synop1 = Nokogiri::HTML(open(soon.url)) # Uses the HTTP 'movieoverview' url
+			doc_synop1 = Nokogiri::HTML(open(soon.url)) # Uses the HTTP 'movieoverview' url
 		rescue
-			redirect_failed = true # An HTTP to HTTPS redirect failed
+			redirect_failed = true # A HTTP to HTTPS redirect failed
 		end
-		
-		if !@doc_synop1.css("a.movie-synopsis-link").any? && !redirect_failed &&
-			@doc_synop1.css("span#SynopsisTextLabel").any?
-			# If not a redirect failure and a READ FULL SYNOPSIS link is not
-			# present and any text is available, use the text for the synopsis
-			soon.synopsis = @doc_synop1.css("span#SynopsisTextLabel").text
+
+		if !redirect_failed
+			if !doc_synop1.css("a.movie-synopsis-link").any? &&
+				doc_synop1.css("span#SynopsisTextLabel").any?
+				# If a READ FULL SYNOPSIS link is not present and any
+				# text is available, use that text for the synopsis
+				soon.synopsis = doc_synop1.css("span#SynopsisTextLabel").text
+			else
+				scrape_plotsummary(soon)
+			end
 		else
-			# Scrape the synopsis using the HTTP 'plotsummary' url
-			# This is also executed after an HTTP to HTTPS redirect failed
-			synop_url = soon.url.sub(/movieoverview/, 'plotsummary')
-			doc_synop2 = Nokogiri::HTML(open(synop_url))
-			soon.synopsis = doc_synop2.css("p.subpage-descriptive-content").text
+			scrape_plotsummary(soon)
 		end
 
 	end
-end	
+
+	def scrape_plotsummary(soon)
+		# Scrape the synopsis using the HTTP 'plotsummary' url
+		# This is also executed after an HTTP to HTTPS redirect failed
+		synop_url = soon.url.sub(/movieoverview/, 'plotsummary')
+		doc_synop2 = Nokogiri::HTML(open(synop_url))
+		soon.synopsis = doc_synop2.css("p.subpage-descriptive-content").text
+		
+	end
+
+end
