d34e9085528605681755891e88fbe12563aa90e0
terryblue99 <terryblue@comcast.net>
2017-05-03 18:51:03 -0500

    Refactored

--- lib/coming_soon/movie.rb
+++ lib/coming_soon/movie.rb
@@ -1,15 +1,20 @@
 
 class ComingSoon::Movie
+	
 	attr_accessor :name, :start_date, :url, :synopsis
 
 	@@movies = []
 
 	def initialize
+
 		@@movies << self
+
 	end	
 
 	def self.movies
+
 		@@movies
+
 	end	
 
 end
