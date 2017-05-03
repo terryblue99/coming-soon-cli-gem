dcf937f8547d43a0bce5537cd184e82cc6fdb823
terryblue99 <terryblue@comcast.net>
2017-05-03 15:55:43 -0500

    refactored

--- lib/coming_soon/cli.rb
+++ lib/coming_soon/cli.rb
@@ -1,35 +1,34 @@
 class ComingSoon::CLI
 
 	def call
-		get_and_list_movies
-		
+		puts ''
+		puts '               **************************'
+		puts '               *   Movies Coming Soon   *'
+		puts '               *     --------------     *'
+		puts '               *      Please wait!      *'
+		puts '               **************************'
+		puts ''
+
+		ComingSoon::Scraper.scrape_movies
+		get_and_list_movies		
 		menu_selection
 	end
 
 	def get_and_list_movies
-		puts ''
-		puts '               **********************'
-		puts '               * Movies Coming Soon *'
-		puts '               *   --------------   *'
-		puts '               *    Please wait!    *'
-		puts '               **********************'
-		puts ''
-
-		@movies = ComingSoon::Movie.movies
-
-		@movies.each.with_index(1) do |movie, i| # set the first index to 1 and use the index number for the movie number
+		
+		ComingSoon::Movie.movies.each.with_index(1) do |movie, i| # set the first index to 1 and use the index number for the movie number
 			puts "#{i}. #{movie.name} - #{movie.start_date}"
 		end
 	end
 
 	def list_saved_movies
 		puts ''
-		puts '               **********************'
-		puts '               * Movies Coming Soon *'
-		puts '               **********************'
+		puts '               **************************'
+		puts '               *   Movies Coming Soon   *'
+		puts '               **************************'
 		puts ''
 
-		@movies.each.with_index(1) do |movie, i| # set the first index to 1 and use the index number for the movie number
+		ComingSoon::Movie.movies.each.with_index(1) do |movie, i| # set the first index to 1 and use the index number for the movie number
 			puts "#{i}. #{movie.name} - #{movie.start_date}"
 		end
 	end
@@ -43,9 +42,9 @@ class ComingSoon::CLI
 
 			input = gets.strip
 
-			if input.to_i > 0 && input.to_i < @movies.size+1
-				puts "* #{@movies[input.to_i - 1].name} - #{@movies[input.to_i - 1].start_date} *"
-				puts @movies[input.to_i - 1].synopsis	
+			if input.to_i > 0 && input.to_i < ComingSoon::Movie.movies.size+1
+				puts "* #{ComingSoon::Movie.movies[input.to_i - 1].name} - #{ComingSoon::Movie.movies[input.to_i - 1].start_date} *"
+				puts ComingSoon::Movie.movies[input.to_i - 1].synopsis	
 			elsif input == 'list'
 				list_saved_movies
 			elsif input == 'exit'
@@ -59,11 +58,11 @@ class ComingSoon::CLI
 
 	def goodbye
 		puts ''
-		puts '               *************************'
-		puts '               * Thank you and goodbye *'
-		puts '               *   -----------------   *'
-		puts '               *    Come back soon!    *'
-		puts '               *************************'
+		puts '               ***************************'
+		puts '               *  Thank you and goodbye  *'
+		puts '               *    -----------------    *'
+		puts '               *     Come back soon!     *'
+		puts '               ***************************'
 		puts ''
 
 		exit
